1434845cbdb05a37d85932dfd66a9701ec8b6d75
Kaden Claunch <kaden.e.claunch@okstate.edu>
2025-02-13 21:05:12 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace KC_Basic_App_GitHub
+{
+    public class Class1
+    {
+        public string Name { get; }
+
+        public Class1()
+        {
+            Name = "Kaden Claunch - The Jack of all Trades";
+        }
+    }
+}
